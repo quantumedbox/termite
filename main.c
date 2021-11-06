fa24a79e3f846f39e840829b81915e997ecf51fa
veclav talica <hectorgreat19@gmail.com>
2021-11-07 01:43:30 +0500

    recursion catching, debugging, hex printing, ronveyor

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "nocrt0/nocrt0c.c"
 
+// todo: make all strings null terminated?
+// todo: hex printing switch
+
 typedef void*         HANDLE;
 typedef const void*   LPCVOID;
 typedef unsigned long DWORD;
@@ -27,10 +30,58 @@ static const char stack_exhausted[]   = "\n!stack exhausted";
 static const char non_ascii_char[]    = "\n!on ASCII char encountered";
 static const char invalid_hex_value[] = "\n!ill-formed hex value";
 
+static _Bool print_as_hex = (_Bool)0; // todo: i don't want it in global scope, should be on stack
+
+static _Bool cstring_compare(const char* first, const char* second) {
+  while ((*first != '\0') && (*second != '\0')) {
+    if (*first != *second)
+      return (_Bool)0;
+    first++;
+    second++;
+  }
+  return (_Bool)1;
+}
+
 static void print(const char* msg, unsigned int len) {
   DWORD chars_written;
   (void)WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), msg, len, &chars_written, NULL);
   (void)chars_written;
+}
+
+static void
+print_value_custom(unsigned char ch,
+                   _Bool as_hex)
+{
+  if (!as_hex) {
+    print((const char*)&ch, 1U);
+  } else {
+    char values[2] = {(ch & 0xF0) >> 4, ch & 0x0F};
+    values[0] += values[0] > 0x9 ? 55 : 48;
+    values[1] += values[1] > 0x9 ? 55 : 48;
+    print((const char*)values, sizeof(values));
+  }
+}
+
+static void
+print_value(unsigned char ch)
+{
+  print_value_custom(ch, print_as_hex);
+}
+
+static void
+print_stack(unsigned char* chars,
+            unsigned int len)
+{
+  const char space = ' ';
+  for (unsigned int i = 0U; i < len; i++) {
+    if (chars[i] < 0x20)
+      print_value_custom(chars[i], (_Bool)1);
+    else
+      print_value_custom(chars[i], (_Bool)0);
+
+    if (i != len - 1U)
+      print(&space, 1U);
+  }
 }
 
 #define put(msg) \
@@ -53,7 +104,23 @@ static void read(char* msg, unsigned int limit, unsigned int* read) {
   }
 }
 
-static void read_input(void) {
+static _Bool
+value_array_compare(unsigned char* restrict first, unsigned int first_len,
+                    unsigned char* restrict second, unsigned int second_len)
+{
+  if (first_len != second_len)
+    return (_Bool)0;
+  for (unsigned int i = 0U; i < first_len; i++) {
+    if (*first != *second)
+      return (_Bool)0;
+  }
+  return (_Bool)1;
+}
+
+static void
+read_input(_Bool print_stack_steps,
+           _Bool catch_infinite_recursion)
+{
   char input[INPUT_LIMIT + 1U];
   unsigned int size;
   read(input, INPUT_LIMIT, &size);
@@ -63,7 +130,16 @@ static void read_input(void) {
 
   unsigned char stack[STACK_LIMIT];
   unsigned int stack_head = 0U;
+
+  // EXPERIMENTAL: required for checking of infinite loops on rewinds
+  // todo: make it compile-time optional?
+  // todo: could be dangerous
+  unsigned char shadow_stack[STACK_LIMIT * catch_infinite_recursion];
+  unsigned char shadow_stack_rewinded_with = 0U;
+  unsigned int shadow_stack_len = 0U;
+
   unsigned int cursor = 0U;
+
   while (1) {
     if (cursor == size)
       break;
@@ -107,13 +183,27 @@ static void read_input(void) {
       }
 
       // 'conveyor belt' operator
-      // place last value on the stack to the beginning
+      // place last value on the stack at the beginning
       case '#': {
         if (stack_head == 0U)
-          crash(unpack_str(stack_exhausted));
-
+          break;
         unsigned char buff = stack[stack_head - 1U];
         for (unsigned int i = 0U; i < stack_head; i++) {
+          unsigned char convey = stack[i];
+          stack[i] = buff;
+          buff = convey;
+        }
+        cursor++;
+        break;
+      }
+
+      // 'ronveyor belt' operator aka 'reverse conveyor'
+      // place first value on the stack at the end
+      case '$': {
+        if (stack_head == 0U)
+          break;
+        unsigned char buff = stack[0U];
+        for (unsigned int i = stack_head; i--;) {
           unsigned char convey = stack[i];
           stack[i] = buff;
           buff = convey;
@@ -193,6 +283,9 @@ static void read_input(void) {
         // todo: define behaviour of overflow
         if (stack_head < 2U)
           crash(unpack_str(stack_exhausted));
+        if (stack[stack_head - 1U] == 0U) {
+          ExitProcess(0L); // for now division by 00 terminates program silently
+        }
         stack[stack_head - 2U] = stack[stack_head - 2U] / stack[stack_head - 1U];
         stack_head--;
         cursor++;
@@ -203,7 +296,7 @@ static void read_input(void) {
       case '<': {
         if (stack_head == 0U)
           crash(unpack_str(stack_exhausted));
-        print((const char*)&stack[stack_head - 1U], 1U);
+        print_value(stack[stack_head - 1U]);
         stack_head--;
         cursor++;
         break;
@@ -255,6 +348,20 @@ static void read_input(void) {
 
         unsigned char n_tokens = stack[stack_head - 1U];
         stack_head--;
+
+        if (catch_infinite_recursion) {
+          if (shadow_stack_rewinded_with != 0U) {
+            if (value_array_compare(shadow_stack, shadow_stack_len, stack, stack_head)) {
+                put("! HALT ON INFINITE RECURSION !");
+                cursor = size;
+                break;
+              }
+          }
+          shadow_stack_rewinded_with = n_tokens;
+          shadow_stack_len = stack_head;
+          for (unsigned int i = stack_head; i--;)
+            shadow_stack[i] = stack[i];
+        }
 
         if (n_tokens != 0U)
           // '[' itself should not count
@@ -383,16 +490,50 @@ static void read_input(void) {
         } else
           stack[stack_head++] = (unsigned char)input[cursor++];
       }
+      if (print_stack_steps == (_Bool)1) {
+        put("|");
+        print_stack(stack, stack_head);
+        put("|\n");
+      }
     }
   }
   put("\n|");
-  print((const char*)stack, stack_head);
-  put("|");
+  print_stack(stack, stack_head);
+  put("|\n");
 }
 
 int main(int argc, char** argv, char** envp) {
   (void)argc;
   (void)argv;
   (void)envp;
-  read_input();
+
+  _Bool print_stack_steps = (_Bool)0;
+  _Bool catch_infinite_recursion = (_Bool)0;
+
+  for (int i = 0; i < argc; i++) {
+    // turn all debug switches
+    if (cstring_compare(argv[i], "-d")) {
+      print_stack_steps = (_Bool)1;
+      catch_infinite_recursion = (_Bool)1;
+
+    // turn on stack step printing
+    } else if (cstring_compare(argv[i], "-s")) {
+      print_stack_steps = (_Bool)1;
+
+    // turn off stack step printing
+    } else if (cstring_compare(argv[i], "-ns")) {
+      print_stack_steps = (_Bool)0;
+
+    // output characters in hex representation
+    } else if (cstring_compare(argv[i], "-h")) {
+      print_as_hex = (_Bool)1;
+
+    // EXPERIMENTAL: try to catch rewinds that don't change state of stack
+    // and thus are most likely infinitely looped
+    } else if (cstring_compare(argv[i], "-l")) {
+      catch_infinite_recursion = (_Bool)1;
+    }
+  }
+
+  read_input(print_stack_steps, catch_infinite_recursion);
 }
